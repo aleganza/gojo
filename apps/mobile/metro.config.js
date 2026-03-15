d5f63fb686657e1abd7b13b974f8a5d169c408c9
Alessio Ganzarolli <ganzarolli.alessio@gmail.com>
2026-03-15 03:02:07 +0100

    stuff

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,18 +1,5 @@
-const path = require('path');
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(__dirname, '..'); // points to deppiu/
-
-const config = getDefaultConfig(projectRoot);
-
-// Include the monorepo root so node_modules can be resolved
-config.watchFolders = [workspaceRoot];
-
-// Tell Metro where to look for node_modules
-config.resolver.nodeModulesPaths = [
-  path.join(projectRoot, 'node_modules'),
-  path.join(workspaceRoot, 'node_modules'),
-];
+const config = getDefaultConfig(__dirname);
 
 module.exports = config;
